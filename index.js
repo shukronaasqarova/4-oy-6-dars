3ba9eea9f83af3ab00081367f4bb4a6ca10b5e61
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-29 00:05:58 +0500

    end

---
+++ index.js
@@ -0,0 +1,81 @@
+// 1 masala
+
+// alert ("birdan 50 gacha bolgan sonlar orasida toqlarining kvadratlarini ekranga chiqarish");
+// let n =50;
+// for (let i = 1; i <= n; i++){
+//     if(i%2==1){
+//         console.log(i*i);
+//     }
+// }
+
+// 2 masala
+
+// alert ("birdan 10 gacha bolgan sonlar kublarini ekranga chiqarish");
+// let n =10;
+// for (let i = 1; i <= n; i++){
+//     console.log(i**3);
+// }
+
+// 3 masala
+
+// alert("1 dan 20 gacha bolgan sonlarni faktorialini chiqarish");
+// let n = 20;
+// let f =0;
+// for(let i = 1; i <= n; i++){
+//     f+=i;
+// } console.log(f);
+
+// 4 masala
+
+// alert ("birdan 30 gacha bolgan sonlarning kvadratlarini yegindisini ekranga chiqarish");
+// let n = 30;
+// let f =0;
+// for(let i = 1; i <= n; i++){
+//     sum=+i*i
+// }
+// console.log(sum);
+
+// 5 masala
+
+// alert("1 dan 20 gacha bolgan sonlarni tublarini consolega chiqarish");
+// let n = 20;
+// let counter = 0;
+// for (let i = 1; i <=n; i++){
+//     if (n%i==0){
+//         counter= counter+1;
+//     } 
+// }
+// if (counter==2){
+//     console.log(counter);
+// } 
+
+
+// 6 masala
+
+// alert("1 dan 100 gacha sonlar orasida 2 ga kopaytirilganda 100dan kichik boladigan sonlarni topish")
+// let n = 100;
+// for (let i = 1; i<100; i++){
+//     console.log(i);
+// }
+
+// 7 masala
+
+// alert("1 dan 100gacha bolgan sonlar orasida 7ga bolinadigan va 5 ga bolinmaydigan sonlarni topish");
+// let n = 100;
+// for (let i = 1; i<100; i++){
+//     if(i % 7 == 0 && i % 5!==0){
+//         console.log(i);
+//     }
+// }
+
+// 8 masala
+
+// alert ("berilgan sonni barcha boluvchilarini toping va ularni yegindisini hisoblang")
+// let n =+prompt("n") ;
+// let sum = 0;
+// for(let i = 1; i <= n; i++){
+//     if(n%2==0){
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
